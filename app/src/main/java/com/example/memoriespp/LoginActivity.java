11fa0aea27e8a159188c25e81777fcca644f34e1
jakubbonczyk <jakubbonczyk@icloud.com>
2025-03-28 22:37:17 +0100

    First commit:  - intro scene  - login scene  - signup scene

---
+++ app/src/main/java/com/example/memoriespp/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.memoriespp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+        AppCompatButton button = findViewById(R.id.button4);
+        button.setOnClickListener(view -> {
+            Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+            startActivity(intent);
+        });
+    }
+}
