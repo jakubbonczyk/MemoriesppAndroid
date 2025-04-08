808a9e55c2c3b242dbd0304a504a35ad0c38699d
OlekZaremba <sarutern.gra@gmail.com>
2025-04-08 16:49:20 +0200

    New structure

---
+++ spring boot/memoriessb/src/main/java/com/example/memoriessb/MemoriessbApplication.java
@@ -0,0 +1,13 @@
+package com.example.memoriessb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoriessbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoriessbApplication.class, args);
+    }
+
+}
