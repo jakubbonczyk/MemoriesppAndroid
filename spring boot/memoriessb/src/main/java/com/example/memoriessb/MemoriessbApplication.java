124b5154adee3223e97c8d9a75451dfc7cfb308b
jakubbonczyk <jakubbonczyk@icloud.com>
2025-04-16 00:36:30 +0200

    configuration and debugging changes in backend

--- spring boot/memoriessb/src/main/java/com/example/memoriessb/MemoriessbApplication.java
+++ spring boot/memoriessb/src/main/java/com/example/memoriessb/MemoriessbApplication.java
@@ -2,12 +2,17 @@ package com.example.memoriessb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class MemoriessbApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(MemoriessbApplication.class, args);
+
+        System.out.println(new BCryptPasswordEncoder().encode("test123"));
+
     }
 
 }
