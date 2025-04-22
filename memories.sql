06cc5dec6d4f3e89c8e310a9ce352fb89f87369b
Adrian Bandiak <byandiaq@gmail.com>
2025-04-22 22:02:02 +0200

    Database update

--- memories.sql
+++ memories.sql
@@ -1,126 +1,263 @@
--- MySQL Workbench Forward Engineering
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 22, 2025 at 10:00 PM
+-- Wersja serwera: 10.4.32-MariaDB
+-- Wersja PHP: 8.2.12
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
--- -----------------------------------------------------
--- Schema memories
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `memories` DEFAULT CHARACTER SET utf8 ;
-USE `memories` ;
 
--- -----------------------------------------------------
--- Table `memories`.`class`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `memories`.`class` (
-  `idclass` INT(11) NOT NULL,
-  `class_name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idclass`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- -----------------------------------------------------
--- Table `memories`.`users`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `memories`.`users` (
-  `idusers` INT(11) NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  `surname` VARCHAR(45) NOT NULL,
-  `role` ENUM('T', 'A', 'S') NOT NULL,
-  `image` BLOB NULL,
-  PRIMARY KEY (`idusers`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
+-- Database: `memories`
+--
 
--- -----------------------------------------------------
--- Table `memories`.`grades`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `memories`.`grades` (
-  `idgrades` INT(11) NOT NULL,
-  `grade` INT(11) NOT NULL,
-  `description` VARCHAR(255) DEFAULT NULL,
-  `users_idstudent` INT(11) NOT NULL,
-  `users_idteacher` INT(11) NOT NULL,
-  `class_idclass` INT(11) NOT NULL,
-  PRIMARY KEY (`idgrades`, `users_idstudent`, `users_idteacher`, `class_idclass`),
-  INDEX `fk_grates_users1_idx` (`users_idstudent` ASC),
-  INDEX `fk_grates_users2_idx` (`users_idteacher` ASC),
-  INDEX `fk_grates_class1_idx` (`class_idclass` ASC),
-  CONSTRAINT `fk_grates_users1` FOREIGN KEY (`users_idstudent`) REFERENCES `memories`.`users` (`idusers`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_grates_users2` FOREIGN KEY (`users_idteacher`) REFERENCES `memories`.`users` (`idusers`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_grates_class1` FOREIGN KEY (`class_idclass`) REFERENCES `memories`.`class` (`idclass`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `memories`.`groups`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `memories`.`groups` (
-  `idgroups` INT(11) NOT NULL,
-  `group_name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idgroups`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
+-- Struktura tabeli dla tabeli `class`
+--
 
--- -----------------------------------------------------
--- Table `memories`.`schedule`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `memories`.`schedule` (
-  `idschedule` INT(11) NOT NULL,
-  `lesson_date` DATE NOT NULL,
-  `start_time` TIME NOT NULL,
-  `end_time` TIME NOT NULL,
-  `class_idclass` INT(11) NOT NULL,
-  `groups_idgroups` INT(11) NOT NULL,
-  `users_idusers` INT(11) NOT NULL,
-  PRIMARY KEY (`idschedule`, `class_idclass`, `groups_idgroups`, `users_idusers`),
-  INDEX `fk_schedule_class1_idx` (`class_idclass` ASC),
-  INDEX `fk_schedule_groups1_idx` (`groups_idgroups` ASC),
-  INDEX `fk_schedule_users1_idx` (`users_idusers` ASC),
-  CONSTRAINT `fk_schedule_class1` FOREIGN KEY (`class_idclass`) REFERENCES `memories`.`class` (`idclass`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_schedule_groups1` FOREIGN KEY (`groups_idgroups`) REFERENCES `memories`.`groups` (`idgroups`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_schedule_users1` FOREIGN KEY (`users_idusers`) REFERENCES `memories`.`users` (`idusers`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `class` (
+  `idclass` int(11) NOT NULL,
+  `class_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
--- -----------------------------------------------------
--- Table `memories`.`sensitive_data`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `memories`.`sensitive_data` (
-  `idsensitive_data` INT(11) NOT NULL,
-  `login` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(256) NOT NULL,
-  `users_idusers` INT(11) NOT NULL,
-  PRIMARY KEY (`idsensitive_data`, `users_idusers`),
-  INDEX `fk_sensitive_data_users_idx` (`users_idusers` ASC),
-  CONSTRAINT `fk_sensitive_data_users` FOREIGN KEY (`users_idusers`) REFERENCES `memories`.`users` (`idusers`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
+-- Dumping data for table `class`
+--
 
--- -----------------------------------------------------
--- Table `memories`.`group_members`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `memories`.`group_members` (
-  `idgroup_members` INT NOT NULL,
-  `groups_idgroups` INT(11) NOT NULL,
-  `users_idusers` INT(11) NOT NULL,
-  PRIMARY KEY (`idgroup_members`, `groups_idgroups`, `users_idusers`),
-  INDEX `fk_group_members_groups1_idx` (`groups_idgroups` ASC),
-  INDEX `fk_group_members_users1_idx` (`users_idusers` ASC),
-  CONSTRAINT `fk_group_members_groups1` FOREIGN KEY (`groups_idgroups`) REFERENCES `memories`.`groups` (`idgroups`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_group_members_users1` FOREIGN KEY (`users_idusers`) REFERENCES `memories`.`users` (`idusers`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO `class` (`idclass`, `class_name`) VALUES
+(1, 'Klasa 1');
 
--- -----------------------------------------------------
--- Dodanie trzech użytkowników
--- -----------------------------------------------------
-INSERT INTO `users` (`idusers`, `name`, `surname`, `role`, `image`) VALUES
-(1, 'Anna', 'Kowalska', 'S', NULL),
-(2, 'Tomasz', 'Nowak', 'T', NULL),
-(3, 'Barbara', 'Wiśniewska', 'A', NULL);
+-- --------------------------------------------------------
 
--- Hasła = test123 (BCrypt: $2a$10$mNl2xuzUAMsLGuxM9msvDeqEu3.DlcP0Rtz0oBmUEoBVhdId7n09m)
+--
+-- Struktura tabeli dla tabeli `grades`
+--
+
+CREATE TABLE `grades` (
+  `idgrades` int(11) NOT NULL,
+  `grade` int(11) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `users_idstudent` int(11) NOT NULL,
+  `users_idteacher` int(11) NOT NULL,
+  `class_idclass` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `grades`
+--
+
+INSERT INTO `grades` (`idgrades`, `grade`, `description`, `users_idstudent`, `users_idteacher`, `class_idclass`) VALUES
+(1, 3, 'test', 1, 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `groups`
+--
+
+CREATE TABLE `groups` (
+  `idgroups` int(11) NOT NULL,
+  `group_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `groups`
+--
+
+INSERT INTO `groups` (`idgroups`, `group_name`) VALUES
+(1, 'Klasa 1');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `group_members`
+--
+
+CREATE TABLE `group_members` (
+  `idgroup_members` int(11) NOT NULL,
+  `groups_idgroups` int(11) NOT NULL,
+  `users_idusers` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `group_members`
+--
+
+INSERT INTO `group_members` (`idgroup_members`, `groups_idgroups`, `users_idusers`) VALUES
+(1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `schedule`
+--
+
+CREATE TABLE `schedule` (
+  `idschedule` int(11) NOT NULL,
+  `lesson_date` date NOT NULL,
+  `start_time` time NOT NULL,
+  `end_time` time NOT NULL,
+  `class_idclass` int(11) NOT NULL,
+  `groups_idgroups` int(11) NOT NULL,
+  `users_idusers` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `sensitive_data`
+--
+
+CREATE TABLE `sensitive_data` (
+  `idsensitive_data` int(11) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `users_idusers` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `sensitive_data`
+--
+
 INSERT INTO `sensitive_data` (`idsensitive_data`, `login`, `password`, `users_idusers`) VALUES
 (1, 'student', '$2a$10$mNl2xuzUAMsLGuxM9msvDeqEu3.DlcP0Rtz0oBmUEoBVhdId7n09m', 1),
 (2, 'teacher', '$2a$10$mNl2xuzUAMsLGuxM9msvDeqEu3.DlcP0Rtz0oBmUEoBVhdId7n09m', 2),
 (3, 'admin', '$2a$10$mNl2xuzUAMsLGuxM9msvDeqEu3.DlcP0Rtz0oBmUEoBVhdId7n09m', 3);
 
--- Przywrócenie ustawień
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
+
+CREATE TABLE `users` (
+  `idusers` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `surname` varchar(255) NOT NULL,
+  `role` enum('T','A','S') NOT NULL,
+  `image` blob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`idusers`, `name`, `surname`, `role`, `image`) VALUES
+(1, 'Anna', 'Kowalska', 'S', NULL),
+(2, 'Tomasz', 'Nowak', 'T', NULL),
+(3, 'Barbara', 'Wiśniewska', 'A', NULL);
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `class`
+--
+ALTER TABLE `class`
+  ADD PRIMARY KEY (`idclass`);
+
+--
+-- Indeksy dla tabeli `grades`
+--
+ALTER TABLE `grades`
+  ADD PRIMARY KEY (`idgrades`,`users_idstudent`,`users_idteacher`,`class_idclass`),
+  ADD KEY `fk_grates_users1_idx` (`users_idstudent`),
+  ADD KEY `fk_grates_users2_idx` (`users_idteacher`),
+  ADD KEY `fk_grates_class1_idx` (`class_idclass`);
+
+--
+-- Indeksy dla tabeli `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`idgroups`);
+
+--
+-- Indeksy dla tabeli `group_members`
+--
+ALTER TABLE `group_members`
+  ADD PRIMARY KEY (`idgroup_members`,`groups_idgroups`,`users_idusers`),
+  ADD KEY `fk_group_members_groups1_idx` (`groups_idgroups`),
+  ADD KEY `fk_group_members_users1_idx` (`users_idusers`);
+
+--
+-- Indeksy dla tabeli `schedule`
+--
+ALTER TABLE `schedule`
+  ADD PRIMARY KEY (`idschedule`,`class_idclass`,`groups_idgroups`,`users_idusers`),
+  ADD KEY `fk_schedule_class1_idx` (`class_idclass`),
+  ADD KEY `fk_schedule_groups1_idx` (`groups_idgroups`),
+  ADD KEY `fk_schedule_users1_idx` (`users_idusers`);
+
+--
+-- Indeksy dla tabeli `sensitive_data`
+--
+ALTER TABLE `sensitive_data`
+  ADD PRIMARY KEY (`idsensitive_data`,`users_idusers`),
+  ADD KEY `fk_sensitive_data_users_idx` (`users_idusers`);
+
+--
+-- Indeksy dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`idusers`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `grades`
+--
+ALTER TABLE `grades`
+  MODIFY `idgrades` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `grades`
+--
+ALTER TABLE `grades`
+  ADD CONSTRAINT `fk_grates_class1` FOREIGN KEY (`class_idclass`) REFERENCES `class` (`idclass`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_grates_users1` FOREIGN KEY (`users_idstudent`) REFERENCES `users` (`idusers`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_grates_users2` FOREIGN KEY (`users_idteacher`) REFERENCES `users` (`idusers`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `group_members`
+--
+ALTER TABLE `group_members`
+  ADD CONSTRAINT `fk_group_members_groups1` FOREIGN KEY (`groups_idgroups`) REFERENCES `groups` (`idgroups`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_group_members_users1` FOREIGN KEY (`users_idusers`) REFERENCES `users` (`idusers`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `schedule`
+--
+ALTER TABLE `schedule`
+  ADD CONSTRAINT `fk_schedule_class1` FOREIGN KEY (`class_idclass`) REFERENCES `class` (`idclass`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_schedule_groups1` FOREIGN KEY (`groups_idgroups`) REFERENCES `groups` (`idgroups`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_schedule_users1` FOREIGN KEY (`users_idusers`) REFERENCES `users` (`idusers`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `sensitive_data`
+--
+ALTER TABLE `sensitive_data`
+  ADD CONSTRAINT `fk_sensitive_data_users` FOREIGN KEY (`users_idusers`) REFERENCES `users` (`idusers`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
