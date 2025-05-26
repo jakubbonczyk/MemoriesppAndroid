1616382b959d012ebb896d2cc792d47708791b1e
OlekZaremba <sarutern.gra@gmail.com>
2025-05-26 21:48:02 +0200

    Added docs for the main class.

--- spring boot/memoriessb/src/main/java/com/example/memoriessb/MemoriessbApplication.java
+++ spring boot/memoriessb/src/main/java/com/example/memoriessb/MemoriessbApplication.java
@@ -4,9 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+/**
+ * Główna klasa uruchamiająca aplikację Spring Boot.
+ * Inicjalizuje kontekst aplikacji i opcjonalnie generuje zakodowane hasło testowe.
+ */
 @SpringBootApplication
 public class MemoriessbApplication {
 
+    /**
+     * Punkt wejścia do aplikacji.
+     *
+     * @param args argumenty linii poleceń (nieużywane)
+     */
     public static void main(String[] args) {
 
         SpringApplication.run(MemoriessbApplication.class, args);
